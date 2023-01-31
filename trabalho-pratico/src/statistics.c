90ade63de9beffb2b12030d17069c3aa42d0ca61
Zé Eduardo <zeeduardo2013@gmail.com>
2023-01-31 17:32:31 +0000

    Q7 se for este o fix fico parvo

--- trabalho-pratico/src/statistics.c
+++ trabalho-pratico/src/statistics.c
@@ -252,14 +252,6 @@ void *order_by_account_age(void *info, char *gender)
                 }
             }
             INFO->order = 1;
-            char* name = malloc(50);
-            sprintf(name,"./Resultados/IdadeD-IdadeU.txt");
-            FILE *resultado = fopen(name, "w");
-            for(int i = 0; i< n;i++){
-                Sexo genero = Gender[i];
-                fprintf(resultado,"%d %d\n",genero.idade_d,genero.idade_u);
-            }
-            fclose(resultado);
         }
     }
     else if(strcmp(gender,"F")==0){
@@ -304,7 +296,7 @@ void *order_by_aval_m(void *info, char *cidade)
     }
     Info **infos = Cidade->info;
     int n = Cidade->num_drivers;
-     if (Cidade->order != 1){
+    if (Cidade->order != 1){
         for (int gap = n/2; gap > 0; gap /= 2)
         {
             for (int i = gap; i < n; i += 1)
@@ -383,7 +375,7 @@ int city_get_num_rides(void *stats_d,char *ID){
     return city->num_rides;
 }
 
-int city_get_info_id(void *stats_d,char *ID, short i){
+int city_get_info_id(void *stats_d,char *ID, int i){
     Stats* stats = (Stats*) stats_d;
     gconstpointer id = (gconstpointer)ID;
     gpointer cityp = g_hash_table_lookup(stats->cities, id);
@@ -391,7 +383,7 @@ int city_get_info_id(void *stats_d,char *ID, short i){
     return city->info[i]->id;
 }
 
-int city_get_info_aval(void *stats_d,char *ID, short i){
+int city_get_info_aval(void *stats_d,char *ID, int i){
     Stats* stats = (Stats*) stats_d;
     gconstpointer id = (gconstpointer)ID;
     gpointer cityp = g_hash_table_lookup(stats->cities, id);
@@ -399,7 +391,7 @@ int city_get_info_aval(void *stats_d,char *ID, short i){
     return city->info[i]->aval;
 }
 
-int city_get_info_num_trips(void *stats_d,char *ID, short i){
+int city_get_info_num_trips(void *stats_d,char *ID, int i){
     Stats* stats = (Stats*) stats_d;
     gconstpointer id = (gconstpointer)ID;
     gpointer cityp = g_hash_table_lookup(stats->cities, id);
